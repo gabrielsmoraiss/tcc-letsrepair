0e4dba8e8b8fe4635cecb25553a0e626c68db815
Gabriel Morais <gabrielsmoraiss@gmail.com>
2016-05-29 14:41:29 -0300

    salvar status deste teste para realizar teste com fusionTables

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,18 @@
 <div class="container">
     <h1>Let's Repair!</h1>
 	<div class="row" >
-		<div style="height: 500px" id="map"></div>
+		<div class="col-sm-8">
+			<div style="height: 500px" id="map"></div>
+		</div>
+		<div class="col-sm-4">
+			<div class="filtro">
+				{!! Form::textField('myLocation', 'Meu Local', '', ['placeholder' => 'Digite o local de onde buscar']) !!}
+
+				<button type="submit"
+                    class="btn btn-success" id="js-filtrar"> Filtrar
+                </button>
+			</div>
+		</div>
 	</div>
 </div>
 
