78ff8bb87c4ee7f5f7d7a6decea135bcfb8cc973
Gabriel Morais <gabrielsmoraiss@gmail.com>
2016-06-05 15:33:09 -0300

    teste com fusion table, funcionando where filtrando, tipo e amplitude da busca

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,8 +10,19 @@
 			<div style="height: 500px" id="map"></div>
 		</div>
 		<div class="col-sm-4">
+			<div class="all">
+				<div class="row" >
+					<div class="col-sm-12">
+						<button type="submit"
+		                    class="btn btn-info" id="js-everythingAroundMe">
+		                    Buscar tudo ao meu Redor
+		                </button>
+					</div>
+				</div>
+			</div>
+			<br>
 			<div class="filtro">
-				{!! Form::textField('myLocation', 'Meu Local', '', ['placeholder' => 'Digite o local de onde buscar']) !!}
+				{!! Form::textField('whenSearch', 'Onde pesquisar', '', ['placeholder' => 'Digite o local de onde buscar']) !!}
 
 				<button type="submit"
                     class="btn btn-success" id="js-filtrar"> Filtrar
--- resources/views/layouts/js.blade.php
+++ resources/views/layouts/js.blade.php
@@ -7,85 +7,111 @@
 
 <script>
 
-var pos;
 var map;
 var service;
 var infowindow;
 var latlng;
 var geocoder;
-function initMap() {
-  var latlng = new google.maps.LatLng(-20.642921,-47.225273); //minha casa
+var circle;
+var layer;
+var radius = 40000;
+var tipoLocal = 'Autorizada';
+var tableid = '1aPLJfYAPlL3L2KVVC-FyZaspqnpv4MWK-dYpgbPS';
 
+function initMap() {
+  latlng = new google.maps.LatLng(-20.642921,-47.225273); //minha casa
+  //console.log(JSON.stringify(latlng));
   map = new google.maps.Map(document.getElementById('map'), {
     center: latlng,
-    zoom: 14
+    zoom: 8
   });
-
-  var layer = new google.maps.FusionTablesLayer({
-    query: {
-      select: '\'Location\'',
-      from: '1BT8PWnY-JBSphaDJ2cTW8W6702xoDXEkS4_C0DC1'
-    }
-  });
-  layer.setMap(map);
 
   infowindow = new google.maps.InfoWindow();
-  service = new google.maps.places.PlacesService(map);
-
-  //var infoWindow = new google.maps.InfoWindow({map: map});
   service = new google.maps.places.PlacesService(map);
   geocoder = new google.maps.Geocoder;
 
 }
-  //Filtro
-  
+//obtem localização do usuario e centraliza mapa nela
+if (navigator.geolocation) {
+	  navigator.geolocation.getCurrentPosition(function(position) {
+	    //se achou o local e o navegador suporta
+	    var pos = { lat: position.coords.latitude, lng: position.coords.longitude };
+	    var $whenSearch = $('#whenSearch');
+	    //Atribui localização encontrada ao input de onde pesquisar
+	    geocoder.geocode({'location': pos}, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+          if (results[1]) {
+          	$whenSearch.val(results[1].formatted_address);
+          } else {
+            console.log('Local não encontrado!');
+          }
+        } else {
+          console.log('Geocoder failed due to: ' + status);
+        }
+      });
+
+	    //centraliza mapa na posição encontrada
+	    map.setCenter(pos);
+
+	  }, function() {
+	    // se não encontrou a localização de nois
+	    console.log('Geolocation não autorizado ou com problemas');
+	  });
+	} else {
+	  // Browser doesn't support Geolocation
+	  console.log('Navegador não suporta Geolocation');
+	}
+
+
+// Buscar todas as Assistências Na posição encontrada do user
+function getEverythingAroundMe() {
+	if (navigator.geolocation) {
+	  navigator.geolocation.getCurrentPosition(function(position) {
+	    //se achou o local e o navegador suporta
+	    var pos = {
+	      lat: position.coords.latitude,
+	      lng: position.coords.longitude
+	    };
+	   	//pesquisa todos os locais em volta..
+	   	markPlacesFromTables(pos);
+
+	  }, function() {
+	    // se não encontrou a localização de nois
+	    console.log('Geolocation não altorizado ou com problemas');
+	  });
+	} else {
+	  // Browser doesn't support Geolocation
+	  console.log('Navegador não suporta Geolocation');
+	}
+
+}
+
+//Busca tudo ao redor
+$('#js-everythingAroundMe').on('click', function() {
+	getEverythingAroundMe();
+});
+
+//Filtro
 $('#js-filtrar').on('click', function() {
-  var address = $('#myLocation').val();
+
+  var address = $('#whenSearch').val();
+	//var meters = $('#kmSearch').val();
+	//var meters = $('#typeAssist').val();
+	  
   geocoder.geocode( { 'address': address}, function(results, status) {
 	  if (status == google.maps.GeocoderStatus.OK) {
+	  	var pos = results[0].geometry.location; 
+	  	//pesquisa os locais em volta..
+	   	markPlacesFromTables(pos, 'Autorizada', 50000);
 
-	    tagMe(results[0].geometry.location);
 	  } else {
 	    console.log("Geocode was not successful for the following reason: " + status);
 	  }
+	
 	});
 });
-
-
-  // Obter minha localização
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      //se achou o local e o navegador suporta
-      var pos = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      
-      geocoder.geocode({'location': pos}, function(results, status) {
-        if (status === google.maps.GeocoderStatus.OK) {
-          if (results[1]) {
-
-           console.log(results[1].formatted_address);
-          } else {
-            window.alert('No results found');
-          }
-        } else {
-          window.alert('Geocoder failed due to: ' + status);
-        }
-      });
-      
-
-      tagMe(pos);
-
-    }, function() {
-      // se não encontrou a localização de nois
-      console.log('Geolocation não altorizado ou com problemas');
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    console.log('Navegador não suporta Geolocation');
-  }
-
+	
+	//marca e centraliza o usuario com a posição dada.
   function tagMe(myLocation) {
 
     map.setCenter(myLocation);
@@ -102,16 +128,45 @@ $('#js-filtrar').on('click', function() {
       infowindow.setContent('Minha Localização');
       console.log(this);
       infowindow.open(map, this);
-    });
-
-    placeSearch(myLocation);
-    
+    });  
   }
 
+  function markPlacesFromTables(myLocation, tipoLocal = 'Autorizada', radius = 50000) {
+
+    console.log(myLocation, tipoLocal, radius);
+
+    //camada  do fusion table
+    layer = new google.maps.FusionTablesLayer({
+	    query: {
+	      select: '\'Location\'',
+	      from: tableid,
+	      where: 'category like \'' + tipoLocal + '\' and ST_INTERSECTS(Location, CIRCLE(LATLNG('
+	      	+ myLocation.lat + ',' + myLocation.lng + '), ' + radius + '))'
+	    }
+	  });
+	  layer.setMap(map);
+
+    var circle = new google.maps.Circle({
+      strokeColor: '#0000ff',
+      strokeOpacity: 0.4,
+      strokeWeight: 1,
+      fillColor: '#0000ff',
+      fillOpacity: 0.07,
+      map: map,
+      center: myLocation,	
+      radius: radius,
+    });
+
+    //marcar posição do user.
+    tagMe(myLocation);
+
+  }
+
+  //pesquisa pelo Places API
   function placeSearch(myLocation) {
 
     //abrangencia das pesquisas e tamanho do circulo
-    var radius = 10000; 
+    var radius = 50000; 
 
     var scopeCircle = new google.maps.Circle({
       strokeColor: '#0000ff',
@@ -125,51 +180,51 @@ $('#js-filtrar').on('click', function() {
     });
 
     var options = {
-    //bounds: map.getBounds(),
+    	//bounds: map.getBounds(),
       keyword: 'Assistência Técnica',
       location: myLocation,
       radius: radius,
       types: ['establishment', 'electronics_store'],
-  };
+  	};
     //service.nearbySearch(options, callback); // locais proximos
     service.radarSearch(options, callback);
-  }
 
-  function callback(results, status) {
-    if (status !== google.maps.places.PlacesServiceStatus.OK) {
-      console.error(status);
-      return;
-    }
-    for (var i = 0, result; result = results[i]; i++) {
-      addMarker(result);
-    }
-  }
+	  function callback(results, status) {
+	    if (status !== google.maps.places.PlacesServiceStatus.OK) {
+	      console.error(status);
+	      return;
+	    }
+	    for (var i = 0, result; result = results[i]; i++) {
+	      addMarker(result);
+	    }
+	  }
 
-  function addMarker(place) {
-  var marker = new google.maps.Marker({
-    map: map,
-    position: place.geometry.location,
-    icon: {
-      url: 'https://cdn2.iconfinder.com/data/icons/snipicons/500/map-marker-48.png',
-      scaledSize: new google.maps.Size(25, 25)
-    }
-  });
+	  function addMarker(place) {
+		  var marker = new google.maps.Marker({
+		    map: map,
+		    position: place.geometry.location,
+		    icon: {
+		      url: 'https://cdn2.iconfinder.com/data/icons/snipicons/500/map-marker-48.png',
+		      scaledSize: new google.maps.Size(25, 25)
+		    }
+		  });
 
-  google.maps.event.addListener(marker, 'click', function() {
-    service.getDetails(place, function(result, status) {
-      if (status !== google.maps.places.PlacesServiceStatus.OK) {
-        console.error(status);
-        return;
-      }
-      console.log(result.types);
-      var tipos = result.types;
-      infowindow.setContent('Nome: ' + result.name + ' Tipo: ' + tipos.join(" , "));
-      infowindow.open(map, marker);
-      console.log(result);
-    });
-  });
-}
+		  google.maps.event.addListener(marker, 'click', function() {
+		    service.getDetails(place, function(result, status) {
+		      if (status !== google.maps.places.PlacesServiceStatus.OK) {
+		        console.error(status);
+		        return;
+		      }
+		      console.log(result.types);
+		      var tipos = result.types;
+		      infowindow.setContent('Nome: ' + result.name + ' Tipo: ' + tipos.join(" , "));
+		      infowindow.open(map, marker);
+		      console.log(result);
+		    });	
+		  });
+		}
 
+  } //fim pesquisa pelo places api
 </script>
 
 
